72556a40e491b3a227ae38161966eaf6361a112f
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-10-19 09:02:52 +0200

    Added the html page and quotes object

---
+++ dom-manipulation/script.js
@@ -0,0 +1,30 @@
+const quoteButton = document.getElementById("newQuote");
+let quotes = [
+  { 
+    text: "The only way to do great work is to love what you do.", 
+    category: "Motivation" 
+  },
+  { 
+    text: "Success is not final, failure is not fatal: it is the courage to continue that counts.", 
+    category: "Perseverance" 
+  },
+  { 
+    text: "Don’t watch the clock; do what it does. Keep going.", 
+    category: "Productivity" 
+  },
+  { 
+    text: "Your time is limited, so don’t waste it living someone else’s life.", 
+    category: "Inspiration" 
+  },
+  { 
+    text: "It always seems impossible until it’s done.", 
+    category: "Determination" 
+  }
+];
+
+function displayRandomQuote(){
+	
+}
+function addQuote(){
+	
+}
